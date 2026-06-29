e0e1361b345daa6ed2bb6858a54ae5aed202b362
root <root@localhost.localdomain>
2026-06-29 06:12:42 +0000

    chore: add gitignore

---
+++ instances/discord-bot/main.py
@@ -0,0 +1,7 @@
+import time
+
+print("Discord Bot Started", flush=True)
+
+while True:
+    print("Heartbeat", flush=True)
+    time.sleep(3)
---
+++ instances/testbot/test.py
@@ -0,0 +1,8 @@
+import time
+
+i = 1
+
+while True:
+    print(f"Tick {i}", flush=True)
+    i += 1
+    time.sleep(5)
